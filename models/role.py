e0883baaca97813b41416c0905542800a67e4d3a
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-06-25 19:32:52 +0700

    role column length

--- models/role.py
+++ models/role.py
@@ -8,7 +8,7 @@ class Role(Base):
     __tablename__ = "role"
 
     id = Column(Integer, primary_key=True, index=True)
-    role = Column(String(length=10), unique=True, nullable=False)
+    role = Column(String(length=100), unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     created_by = Column(Integer)
